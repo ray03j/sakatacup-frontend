50e915181c79d65065bca503e3b42f20bcd7385d
ray03j <eihouse.potato@outlook.jp>
2025-02-22 21:42:45 +0900

    必要なページの追加

--- app/game/[gameId]/setting/layout.tsx
+++ app/game/[gameId]/setting/layout.tsx
@@ -1,4 +1,4 @@
-export default function AboutLayout({
+export default function GameSettingLayout({
   children,
 }: {
   children: React.ReactNode;
---
+++ app/game/[gameId]/setting/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function GameSettingPage() {
+  return (
+    <div>
+      <h1 className={title()}>Game Setting</h1>
+    </div>
+  );
+}
